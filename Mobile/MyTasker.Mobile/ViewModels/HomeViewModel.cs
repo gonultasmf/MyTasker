ddf1964d78eb930959bbc638245767e3839590b1
Mustafa Gönültaş <gonultasmf@hotmail.com>
2023-11-19 00:18:33 +0300

    appicon and app name changed. HomePage fixes

--- Mobile/MyTasker.Mobile/ViewModels/HomeViewModel.cs
+++ Mobile/MyTasker.Mobile/ViewModels/HomeViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using FmgLib.HttpClientHelper;
 using MyTasker.Core.Models;
 
@@ -12,14 +13,37 @@ public partial class HomeViewModel : BaseViewModel
     [ObservableProperty]
     private List<TaskModel> _tasks;
 
+    [ObservableProperty]
+    private string _userText;
+
     public HomeViewModel()
     {
         GetHomePageDetails();
+    }
+
+    [RelayCommand]
+    public async Task Search(string searchText)
+    {
+        var tasks = await HttpClientHelper.SendAsync<List<TaskModel>>(App.BaseUrl + $"/Task/GetAllSearch/{searchText}", HttpMethod.Get);
     }
 
     private async void GetHomePageDetails()
     {
         Tasks = await HttpClientHelper.SendAsync<List<TaskModel>>(App.BaseUrl + "/Task/GetAllWithToday", HttpMethod.Get);
         Settings = await HttpClientHelper.SendAsync<SettingsModel>(App.BaseUrl + "/Settings", HttpMethod.Get);
+        UserText = GetUserText();
+    }
+
+
+    private string GetUserText()
+    {
+        var time = DateTime.Now.Hour;
+
+        if (time >= 5 && time < 12)
+            return $"Good Morning, {Settings.UserName}!";
+        else if (time >= 12 && time < 18)
+            return $"Good Afternoon, {Settings.UserName}!";
+        else
+            return $"Good Night, {Settings.UserName}!";
     }
 }
